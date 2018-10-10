1fb6926162da438b843ff0dda43d72696a05af8d
LarryLeo <esharemore@gmail.com>
2018-10-10 19:25:19 +0800

    project init

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div class="home">Home</div>
+    <router-link to="/list" class="home">列表页</router-link>
+  </div>
+
+</template>
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+<style>
+  .home {
+    font-size: 30px;
+  }
+</style>
