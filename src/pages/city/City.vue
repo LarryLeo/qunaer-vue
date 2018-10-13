fa180b0692aee9e12ae5d41e20554170cf4d1930
LarryLeo <esharemore@gmail.com>
2018-10-13 22:00:15 +0800

    city-list layout finished

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,16 +2,23 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
+    <city-list></city-list>
+    <city-alhapbet></city-alhapbet>
   </div>
 </template>
 <script>
 import CityHeader from './components/Header.vue'
 import CitySearch from './components/Search.vue'
+import CityList from './components/List.vue'
+import CityAlphabet from './components/Alphabet.vue'
+
 export default {
   name: 'City',
   components: {
     CityHeader: CityHeader,
-    CitySearch: CitySearch
+    CitySearch: CitySearch,
+    CityList: CityList,
+    CityAlhapbet: CityAlphabet
   }
 }
 </script>
