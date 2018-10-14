f63bb2e6161a19b0099a365e0b48c9b872832c33
LarryLeo <esharemore@gmail.com>
2018-10-14 13:50:29 +0800

    city-ajax added

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,11 +2,12 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
-    <city-list></city-list>
-    <city-alhapbet></city-alhapbet>
+    <city-list :cities="cities" :hotCities="hotCities"></city-list>
+    <city-alhapbet :cities="cities"></city-alhapbet>
   </div>
 </template>
 <script>
+import axios from 'axios'
 import CityHeader from './components/Header.vue'
 import CitySearch from './components/Search.vue'
 import CityList from './components/List.vue'
@@ -19,6 +20,29 @@ export default {
     CitySearch: CitySearch,
     CityList: CityList,
     CityAlhapbet: CityAlphabet
+  },
+  data () {
+    return {
+      cities: {},
+      hotCities: []
+    }
+  },
+  methods: {
+    getCityInfo () {
+      axios.get('/api/city.json')
+        .then(this.getCityInfoSucc)
+    },
+    getCityInfoSucc (res) {
+      res = res.data
+      if (res.ret && res.data) {
+        const data = res.data
+        this.cities = data.cities
+        this.hotCities = data.hotCities
+      }
+    }
+  },
+  mounted () {
+    this.getCityInfo()
   }
 }
 </script>
