b5d742fab7a63317c16ab5e87bf8f83677eb3755
LarryLeo <esharemore@gmail.com>
2018-10-14 18:24:49 +0800

    搜索功能完成

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,11 +1,64 @@
 <template>
-  <div class="search">
-    <input type="text" placeholder="请输入城市名或拼音" class="search-input">
+  <div>
+    <div class="search">
+      <input type="text" placeholder="请输入城市名或拼音" class="search-input" v-model="keyword">
+    </div>
+    <div class="search-content" ref="search" v-show="keyword">
+      <ul>
+        <li
+          class="search-item border-bottom"
+          v-for="(item, index) in resultList"
+          :key="index"
+        >{{item.name}}</li>
+        <li class="search-item border-bottom" v-show="hasNoData">没有找到匹配数据</li>
+      </ul>
+    </div>
   </div>
 </template>
 <script>
+import Bscroll from 'better-scroll'
 export default {
-  name: 'CitySearch'
+  name: 'CitySearch',
+  props: {
+    cities: Object
+  },
+  data () {
+    return {
+      keyword: '',
+      resultList: [],
+      time: null
+    }
+  },
+  watch: {
+    keyword () {
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      if (!this.keyword) {
+        this.resultList = []
+        return
+      }
+      this.timer = setTimeout(() => {
+        const result = []
+        for (let i in this.cities) {
+          this.cities[i].forEach((value) => {
+            if (value.spell.indexOf(this.keyword) > -1 || value.name.indexOf(this.keyword) > -1) {
+              result.push(value)
+            }
+          })
+        }
+        this.resultList = result
+      }, 100)
+    }
+  },
+  mounted () {
+    this.scroll = new Bscroll(this.$refs.search)
+  },
+  computed: {
+    hasNoData () {
+      return !this.resultList.length
+    }
+  }
 }
 </script>
 <style lang="stylus" scoped>
@@ -25,4 +78,20 @@ export default {
     border-radius 0.06rem
     color #666
     padding 0 0.1rem
+
+.search-content
+  position absolute
+  z-index 1
+  overflow hidden
+  top 1.58rem
+  left 0
+  right 0
+  bottom 0
+  background #eee
+
+  .search-item
+    line-height 0.62rem
+    padding-left 0.2rem
+    background-color #fff
+    color #666
 </style>
